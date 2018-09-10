dbc98be52df601a9f788350df8ea1c25a8cac288
iac-nyc <007ifte@gmail.com>
2018-09-10 11:26:09 -0400

    Employee_Directory

---
+++ README.md
@@ -0,0 +1,28 @@
+## Project by: Iftekhar A Chowdhury
+## Project Title: Use a Public API to Create an Employee Directory
+## Project Requirements:
+```
+    1) Get and display 12 random users from The Random User Generator API
+        a) Using photos and information that the API provides, you’ll display 12 users, along with some basic information:
+            i)   Image
+            ii)  First and Last Name
+            iii) Email
+            iv)  City
+    2) Create a modal window that will pop up when any part of the user’s row is clicked. The following details should display in the modal window:
+        a) Image
+        b) Name
+        c) Username
+        d) Email
+        e) Cell Number
+        f) Detailed Address, including street name and number, city, country and post code.
+        g) Birthdate
+    3) Structure and style the user directory so that it roughly matches the provide mockup.
+        a) Display the users in a grid or table
+        b) Add a hover state to the rows of the user table.
+        c) Make sure there’s a way to close the modal window
+```
+## Exceeds Expectations Requirements:
+```
+    1) Add a way to filter the directory by name or username. To do this, you’ll need to request a random user nationality that will only return data in the English alphabet.
+       Note: you don't have to rely on the API to return search results. You'll need to write functionality that filters results once they already on the page.
+    2) Add a way to move back and forth between employee detail windows when the modal window is open.
---
+++ js/app.js
@@ -0,0 +1,135 @@
+$(document).ready(() => {
+
+    //Rearrange the received dob to mm/dd/yyy
+    const setBirthday = (birthday) => {
+        let birthdayFormat = new Date(birthday).toLocaleDateString('en-US', { year: 'numeric', month: 'numeric', day: 'numeric' });
+        return birthdayFormat;
+    }
+
+    //Employee Info Div
+    const employeeData = (data) => {
+        //data.results[0] - set variable for the needed information
+        let image = data.picture.large;
+        let name = `${data.name.first} ${data.name.last}`;
+        let email = data.email;
+        let city = data.location.city;
+        let username = data.login.username;
+        let cell = data.phone;
+        let fullAddress = `${data.location.street}, ${city}, ${data.location.state} ${data.location.postcode}`;
+        let birthday = setBirthday(data.dob.date);
+        let nationality= data.nat;
+        
+        //build the list items and the modal window divs
+        let employeeList = `<li class="employee">
+                              <a data-modal-open="${username}" href="#">
+                                <div class="images"><img src="${image}" alt="profile image">
+                                </div>
+                                <div class="content">
+                                  <h3 class="capitalize">${name}</h3>
+                                  <p>${email}</p>
+                                  <p class="capitalize">${city}</p>
+                                </div>
+                              </a>
+                            </li>`;
+        let employeeModal = `<div class="modal" data-modal="${username}">
+                              <div class="modal-content">
+                                  <a class="modal-close" data-modal-close="${username}" href="#">x</a>
+                                  <a id="prev" class="arrows" href="#" data-arrow="prev"><</a>
+                                  <a class="arrows" id="next" href="#" data-arrow="next">></a>
+                                  <img src="${image}" alt="profile image">
+                                  <h3 class="capitalize">${name}</h3>
+                                  <p>${email}</p><p class="capitalize">${city}</p>
+                              <div class="line-break"></div>
+                                  <h3>Employee Details</h3>
+                                  <p>${username}</p><p>${cell}</p>
+                                  <p class="capitalize">${fullAddress}</p>
+                                  <p>${birthday}</p><p>${nationality}</p>
+                             </div>
+                             </div>`;
+        employeeList += employeeModal;
+        //append them to the page
+        $('#list').append(employeeList);
+    }
+
+    //call a request to receive 12 fake employees for the mockup
+    $.ajax({
+      url: 'https://randomuser.me/api/?nat=us&results=12',
+      dataType: 'json',
+      success: function(data) {
+          //interate through the data received to build out the page
+          for( let i = 0; i < data.results.length; i++) {
+              employeeData(data.results[i]);
+          }
+      }
+    });
+
+    /////////////////MODAL WINDOWS////////////////////////
+
+    //on click of the li element
+    $('body').on('click', '[data-modal-open]', (e) => {
+        //grab the username of the li's employee
+        let target = $(e.currentTarget).attr('data-modal-open');
+        //show matching modal window
+        $(`[data-modal="${target}"]`).show();
+    });
+
+    //on click of the modal X close button
+    $('body').on('click', '[data-modal-close]', (e) => {
+        //grab the username of the modals' employee
+        let target = $(e.target).attr('data-modal-close');
+        //hide matching modal window
+        $(`[data-modal="${target}"]`).hide();
+        //prevent default click events
+        e.preventDefault();
+    });
+
+    //on click outside of the modal - hide the currently open modal
+    $('body').on('click', '.modal', (e) => {
+        //grab the username of the modals' employee
+        let target = $(e.target).find('a').attr('data-modal-close');
+        //hide matching modal window
+        $(`[data-modal="${target}"]`).hide();
+        //prevent default click events
+        e.preventDefault();
+    });
+
+    ////////////////////SEARCH/////////////////
+    //Search through the students and find those that match the search
+    const findPerson = () => {
+        $('#none').hide();
+        //get the item that was searched
+        let searchItem = $('#search input').val().toLowerCase();
+        //find all items that contain the searched item
+        let returnedItems = $(`li:contains('${searchItem}')`);
+        //return the search box value to empty
+        $('#search input').val('');
+        //return the results to create the page
+        $('.employee').hide();
+        if (returnedItems[0] === undefined) {
+            $('#list').append('<p id="none">Search Result : Not found.</p>');
+        } else {
+            returnedItems.show();
+        }
+    }
+
+    $('#search button').click(() => {
+        findPerson();
+    });
+
+    $('#search input').bind('keypress', (event) => {
+        if (event.keyCode === 13) {
+            findPerson();
+        }
+    });
+
+    ///////////////////////////ARROW TO CHANGE MODALS///////////////////////////
+    //capture click event on the modal
+    $('body').on('click','#prev', (e) => {
+        $(e.target).closest('.modal').hide().prevAll('.modal:first').show();
+    });
+
+    $('body').on('click','#next', (e) => {
+        $(e.target).closest('.modal').hide().nextAll('.modal:first').show();
+    });
+
+});
